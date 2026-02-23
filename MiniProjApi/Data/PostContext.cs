532ee383161d49357f747822e4a4fa33cae2dc23
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2026-02-23 10:38:35 +0100

    aaa

--- MiniProjApi/Data/PostContext.cs
+++ MiniProjApi/Data/PostContext.cs
@@ -8,12 +8,26 @@ namespace MiniProjApi.Data
         public DbSet<User> Users => Set<User>();
         public DbSet<Posts> Post => Set<Posts>();
 
+        public string DbPath { get; }
+
+        public PostsContext()
+        {
+            DbPath = "bin/MiniProjApi.db";
+        }
 
         public PostsContext(DbContextOptions<PostsContext> options)
             : base(options)
         {
             // Den her er tom. Men ": base(options)" sikre at constructor
             // på DbContext super-klassen bliver kaldt.
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Posts>().ToTable("Posts");
         }
     }
 }

7d5d1089318334954efd0e8583aedfa68e5afdaf
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2026-02-23 09:34:25 +0100

    context db

---
+++ MiniProjApi/Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniProjApi.Model;
+
+namespace Data
+{
+    public class PostsContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Posts> Post => Set<Posts>();
+
+
+        public PostsContext(DbContextOptions<PostsContext> options)
+            : base(options)
+        {
+            // Den her er tom. Men ": base(options)" sikre at constructor
+            // på DbContext super-klassen bliver kaldt.
+        }
+    }
+}

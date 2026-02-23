3c838dc3838aa604e1a9a74d6e725faf8572c7e1
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2026-02-23 09:52:41 +0100

    cors

--- MiniProjApi/Program.cs
+++ MiniProjApi/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Tilføj CORS service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowMyOrigin",
+        policy =>
+        {
+            policy.WithOrigins("https://example.com") // Din frontend URL
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Brug CORS
+app.UseCors("AllowMyOrigin");
+
+app.MapControllers();
+
+app.Run();
